73cb05db310a07ec46d00ce860a806a245e16214
Victor Pinto <victorpintowake@gmail.com>
2023-05-10 21:27:07 -0300

    first commit

---
+++ leerData/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace leerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
